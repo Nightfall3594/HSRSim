5f336c85ed7bb25e3a8887e1d64c7a1d78e2bc3a
Nightfall3594 <briankristoferperez28@gmail.com>
2025-05-31 18:52:07 +0800

    Added command-safety and manual mapping via typing

--- discord/commands/slash_commands.py
+++ discord/commands/slash_commands.py
@@ -1,7 +1,10 @@
-from typing import Literal, cast
+from typing import Literal, cast, Union
+from pydantic import Field
 from discord.base_classes import SlashCommand
 from discord.components.messages import DiscordMessage
 from discord.components.misc import DiscordContext
+from discord.commands.subcommands import *
+
 
 
 class Greet(SlashCommand):
@@ -13,8 +16,12 @@ class Greet(SlashCommand):
 
 
 class Calculate(SlashCommand):
-
+    """
+    Calculate something.
+    Currently maps to either AV or Turns.
+    """
     name: Literal["calculate"]
+    options: list[Union[CalculateTurns, CalculateAV]] = Field(discriminator="name")
 
     def execute(self, context: DiscordContext) -> DiscordMessage:
         return self.sub_command.execute()

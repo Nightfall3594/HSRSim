328180305fbc1c1db15bd62e799a4914839e6190
Nightfall3594 <briankristoferperez28@gmail.com>
2025-06-13 16:04:17 +0800

    refactor: change response models to webhook methods

--- src/discord/interactions/interaction_responses.py
+++ src/discord/interactions/interaction_responses.py
@@ -1,18 +1,51 @@
-from typing import Literal, Optional
+from typing import Literal, Optional, Any
 
 from pydantic import BaseModel
+import httpx, os
 
-class DiscordMessageContent(BaseModel):
-    content: str
+from src.discord.interactions.component_message import ComponentMessage
 
-class InteractionResponse(BaseModel):
-    type: Literal[1, 4, 5, 6, 7, 8, 9]
-    data: Optional[DiscordMessageContent] = None
 
-    @classmethod
-    def generic_message(cls, text: str):
-        return cls(type=4, data=DiscordMessageContent(content=text))
+class CallBackResponse(BaseModel):
+    """
+    Callback responses
+    These are a set of methods for responding
+    """
 
-    @classmethod
-    def deferred_message(cls, text: str):
-        return cls(type=5)
+    @staticmethod
+    def generic_message(message: str, token: str):
+        """
+        For sending your generic message responses.
+        Bear in mind this uses the callback webhook
+        """
+        httpx.post(
+            url=f"https://discord.com/api/v10/webhooks/{os.environ.get('APPLICATION_ID')}/{token}/callback",
+            headers={'Content-Type': 'application/json'},
+            json={"type": 4, "data": {"content": message}}
+        )
+
+
+    @staticmethod
+    def deferred_message(interaction_id: str, token: str):
+        """
+        For sending deferred messages.
+        This sends a "bot is thinking..." response,
+        and sending a follow-up to this message edits it
+        """
+        httpx.post(
+            url=f"https://discord.com/api/v10/interactions/{interaction_id}/{token}/callback",
+            headers={'Content-Type': 'application/json'},
+            json={'type': 5}
+        )
+
+
+
+class FollowUpResponse(BaseModel):
+
+    @staticmethod
+    def send_followup(message: BaseModel, token: str):
+        httpx.post(
+            url=f"https://discord.com/api/v10/webhooks/{os.environ.get('APPLICATION_ID')}/{token}",
+            headers={'Content-Type': 'application/json'},
+            json= message.model_dump()
+        )
